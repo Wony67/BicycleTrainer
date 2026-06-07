9f604476e8aa4ac865754c06b57595116a91ae98
wony67 <wony607@gmail.com>
2026-06-08 01:12:37 +0900

    Use icon for map locate button

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bicycle-trainer-v11";
+const CACHE_NAME = "bicycle-trainer-v12";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
