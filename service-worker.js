2a2053607862baba338c6cd4aba2a42bc19034b7
wony67 <wony607@gmail.com>
2026-06-09 17:23:35 +0900

    Hide record delete controls by default

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bicycle-trainer-v27";
+const CACHE_NAME = "bicycle-trainer-v28";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./icon.svg", "./version.json"];
 
 self.addEventListener("install", (event) => {
